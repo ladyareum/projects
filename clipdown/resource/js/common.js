d04b7f4fa85fa5f021f38b5e72961b31cc4ee61c
ladyareum <ladyareum84@gmail.com>
2020-08-30 01:32:13 +0900

    clipdown update

--- clipdown/resource/js/common.js
+++ clipdown/resource/js/common.js
@@ -25,13 +25,37 @@ $(function() {
 });
 
 
-// //jQuery to collapse the navbar on scroll
-// $(window).scroll(function() {
-//     if ($(".navbar").offset().top > 100) {
-//         $(".navbar-fixed-top").addClass("top-nav-collapse");
-//             $(".navbar-fixed-top").removeClass("navbar-transparent");
-//     } else {
-//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-//       $(".navbar-fixed-top").addClass("navbar-transparent");
-//     }
-// });
+// layerPopup
+function popupOpenClose(popup) {
+	
+	/* Add div inside popup for layout if one doesn't exist */
+	if ($(".wrapper").length == 0){
+		$(popup).wrapInner("<div class='wrapper'></div>");
+	}
+	
+	/* Open popup */
+	$(popup).show();
+
+	/* Close popup if user clicks on background */
+	$(popup).click(function(e) {
+		if ( e.target == this ) {
+			if ($(popup).is(':visible')) {
+				$(popup).hide();
+			}
+		}
+	});
+
+	/* Close popup and remove errors if user clicks on cancel or close buttons */
+	$(popup).find("button[name=close]").on("click", function() {
+		if ($(".formElementError").is(':visible')) {
+			$(".formElementError").remove();
+		}
+		$(popup).hide();
+	});
+}
+
+$(document).ready(function () {
+	$("[data-js=open]").on("click", function() {
+		popupOpenClose($(".popup"));
+	});
+});
